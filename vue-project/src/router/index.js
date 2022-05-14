0d0b5b4d90c4c6bd379f165672cc4afa925ceb3f
younge12 <jey3382@nate.com>
2022-05-14 01:28:42 -0700

    05 hello world

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DataBinding from '../views/DataBinding.vue'
 
 const routes = [
   {
@@ -14,7 +15,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
+  },
 ]
 
 const router = createRouter({
